4e9de94c68f47f64b9b65fd7b76293af869c7d57
s3155 <yutaka.7sn@gmail.com>
2023-07-28 17:07:41 +0000

    Finish add-user-attendances

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,17 @@
 class UsersController < ApplicationController
-before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
-before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
-before_action :correct_user, only: [:edit, :update]
-before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
+  before_action :set_one_month, only: :show
 
   def index
     @users = User.paginate(page: params[:page])
   end
 
-  def show
-  end
+ def show
+  @worked_sum = @attendances.where.not(started_at: nil).count
+end
 
   def new
     @user = User.new
@@ -47,23 +49,23 @@ before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info
   def edit_basic_info
   end
 
-def update_basic_info
-  if @user.update_attributes(basic_info_params)
-    flash[:success] = "#{@user.name}の基本情報を更新しました。"
-  else
-    flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
+  def update_basic_info
+    if @user.update_attributes(basic_info_params)
+      flash[:success] = "#{@user.name}の基本情報を更新しました。"
+    else
+      flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
+    end
+    redirect_to users_url
   end
-  redirect_to users_url
-end
 
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
     end
-    
+
     def basic_info_params
-    params.require(:user).permit(:department, :basic_time, :work_time)
+      params.require(:user).permit(:department, :basic_time, :work_time)
     end
 
     # beforeフィルター
